7582442adc930563f3e87656725ef55b34e18c8c
Satish Raja <satishraja4u@gmail.com>
2024-02-21 18:33:39 +0530

    21022024 6:33pm

---
+++ src/main/java/pageObjects/LoginPage.java
@@ -0,0 +1,51 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import pageBase.BasePage;
+
+public class LoginPage extends BasePage{
+	WebDriver driver ;
+	public LoginPage(WebDriver driver) {
+		super(driver);
+		this.driver = driver ;
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(id="userEmail")
+	WebElement userEmail ;
+	
+	@FindBy(id="userPassword")
+	WebElement userPassword ;
+	
+	@FindBy(id="login")
+	WebElement submit ;
+	
+	@FindBy(id ="toast-container")
+	WebElement loginErrorMessage ;
+	
+	By loginPageMessage = By.id("toast-container");
+//	By loginPageMessage = By.xpath("//div[@role='alert']");
+	
+	
+	public ProductCataloguePage LoginApplication(String userName, String password) {
+		userEmail.sendKeys(userName);
+		userPassword.sendKeys(password);
+		submit.click();
+	System.out.println("Successfully Login into Application");
+	return new ProductCataloguePage(driver);
+	}
+	
+	public void goTo() {
+    	driver.get("https://rahulshettyacademy.com/client/");
+	}
+	
+	public String loginErrorValidation() {
+		waitForElementToAppear(loginPageMessage);	
+		return loginErrorMessage.getText();
+	}
+}
---
+++ src/main/java/pageObjects/ProductCataloguePage.java
@@ -0,0 +1,39 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import pageBase.BasePage;
+
+public class ProductCataloguePage  extends BasePage{
+	WebDriver driver ;
+	public ProductCataloguePage(WebDriver driver) {
+		super(driver);
+		this.driver = driver ;
+		PageFactory.initElements(driver,this);
+	}
+
+
+	@FindBy (xpath = "//button[@routerlink='/dashboard/cart']") 
+	WebElement buttonAddToCart ;
+
+	
+	@FindBy(css =".logo-holder.logo-7" )
+	WebElement productPage;
+	
+	By toastMessage = By.id("toast-container");
+	By productCatalogue = By.tagName("app-dashboard");
+	
+	public CartPage addProductToCart(String productName) {
+		waitForElementToAppear(productCatalogue);
+		driver.findElement(By.xpath("//b[text()='" + productName + "']/parent::h5/following-sibling::button[2]")).click();
+		waitForElementToAppear(toastMessage);
+		waitForElementToDisappear(toastMessage);
+		goToCart();
+		System.out.println("Successfullt added Product: "+ productName +" To Cart");
+		return new CartPage(driver);		
+	}
+}
